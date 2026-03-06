590c69066db91342ae6e6d5d736202eb2386df11
Admin Stagiaire <o.courbe@gmail.com>
2026-03-06 15:29:31 +0100

    WTF

--- webapi-cicd/obj/Debug/net10.0/webapi-cicd.AssemblyInfo.cs
+++ webapi-cicd/obj/Debug/net10.0/webapi-cicd.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("webapi-cicd")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+63dfb18080b4236c7fc0937e0baad30aff2754d5")]
 [assembly: System.Reflection.AssemblyProductAttribute("webapi-cicd")]
 [assembly: System.Reflection.AssemblyTitleAttribute("webapi-cicd")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
